d19f71bffb6ae89c472b1c8c57984913b1620d38
Alius <alexa.glinsoo@gmail.com>
2020-03-26 00:38:46 -0300

    page login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  img: string = "./assets/image/camera.jpg";
+
   constructor() { }
 
   ngOnInit() {
